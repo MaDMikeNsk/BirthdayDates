b2b0d9faab55ee71f6d8ea5b1ed6dd38905b8a7d
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-02-02 15:18:35 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,53 @@
 1 ­- 13.02.1990 -­ Tue
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
+import java.util.Locale;
+import java.text.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final DateFormat BIRTHDAY_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+    static{
+        BIRTHDAY_FORMAT.setLenient(true);
+    }
+
+    private static boolean isValid(String date){  //Проверка вводимой даты на допустимость (например: 31 февраля)
+        try {
+            return BIRTHDAY_FORMAT.format(BIRTHDAY_FORMAT.parse(date)).equals(date);
+        }catch (ParseException ex){
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         Calendar rightNow = Calendar.getInstance();
         Calendar birthDay = Calendar.getInstance();
         DateFormat birthdayFormat = new SimpleDateFormat("dd.MM.yyy");
         DateFormat dayName = new SimpleDateFormat("E");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputDate = "";
 
-        int birthYear=1985;  //My birthday data
-        int birthdayDay = 21;
-        int birthMonth = Calendar.JULY;
+        do {
+            System.out.println("Введите дату рождения в формате дд.мм.гггг: ");
+            inputDate = reader.readLine();
+        } while (!isValid(inputDate));
+
+        String [] parsedDate = inputDate.split("\\.");
+        int birthdayDay = Integer.parseInt(parsedDate[0]);
+        int birthMonth = Integer.parseInt(parsedDate[1])-1;
+        int birthYear= Integer.parseInt(parsedDate[2]);
+        Locale.setDefault(Locale.ENGLISH);
+        birthDay.set(birthYear,birthMonth,birthdayDay);
 
         int i=0;
-        birthDay.set(birthYear,birthMonth,birthdayDay);
         while (birthDay.before(rightNow)) {
-            System.out.println(i + ": " + birthdayFormat.format(birthDay.getTime()) + " - " + dayName.format(birthDay.getTime()));
+            System.out.println(i + " - " + birthdayFormat.format(birthDay.getTime()) + " - " + dayName.format(birthDay.getTime()));
             birthYear++;
             i++;
             birthDay.set(birthYear,birthMonth,birthdayDay);
